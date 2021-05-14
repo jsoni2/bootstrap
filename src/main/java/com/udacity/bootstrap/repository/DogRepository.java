c38175e45d40733c2f7af40fce4680072a346c58
Janak Soni <janaksoni91@gmail.com>
2021-05-14 15:00:56 -0500

    Update DogRepository.java

--- src/main/java/com/udacity/bootstrap/repository/DogRepository.java
+++ src/main/java/com/udacity/bootstrap/repository/DogRepository.java
@@ -1,10 +1,21 @@
 package com.udacity.bootstrap.repository;
 
 import com.udacity.bootstrap.entity.Dog;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by janaksoni on 5/14/21.
  */
 public interface DogRepository extends CrudRepository<Dog, Long> {
+    @Query("select d.id, d.breed from Dog d where d.id=:id")
+    String findBreedById(Long id);
+
+    @Query("select d.id, d.breed from Dog d")
+    List<String> findAllBreed();
+
+    @Query("select d.id, d.name from Dog d")
+    List<String> findAllName();
 }
